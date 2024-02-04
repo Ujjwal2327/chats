89e92db58cc1d6d4e7cd90cf0355e285e8adad30
Ujjwal Maheshwari <ujjwal2327@gmail.com>
2024-02-04 15:31:08 +0530

    ui

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Conversation = () => {
+  return (
+    <>
+      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+        {/* avatar */}
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img src="https://avatar.iran.liara.run/public" alt="user avatar" />
+          </div>
+        </div>
+
+        {/* username */}
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">John Doe</p>
+            <span className="text-xl">🌟</span>
+          </div>
+        </div>
+      </div>
+
+      {/* divider */}
+      <div className="divider my-0 py-0 h-1" />
+    </>
+  );
+};
+
+export default Conversation;
